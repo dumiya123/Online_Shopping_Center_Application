54042979679184ffe08a1a3fe7e9bc095e20a27d
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-12-15 20:50:18 +0530

    first_commit

---
+++ src/console_application/WestminsterShoppingManager.java
@@ -0,0 +1,19 @@
+package console_application;
+
+
+/**
+ COPYRIGHT (C) Dumindu Induwara Gamage-20221168-w1953846-dumindu.20221168@iit.ac.lk. All Rights Reserved.
+ Object-Oriented Programming Coursework L5 sem 1
+ @author Dumindu Induwara Gamage
+ @version 1 Console application
+ */
+
+/**
+ * This is the main class
+ */
+public class WestminsterShoppingManager
+{
+
+
+
+}
