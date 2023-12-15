0ff7242e13c59b223638567406118a9e5af1fdfd
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-12-15 23:50:42 +0530

    initial commit

--- src/console_application/WestminsterShoppingManager.java
+++ src/console_application/WestminsterShoppingManager.java
@@ -1,8 +1,15 @@
 package console_application;
 
 
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 
 /**
  COPYRIGHT (C) Dumindu Induwara Gamage-20221168-w1953846-dumindu.20221168@iit.ac.lk. All Rights Reserved.
@@ -18,7 +25,13 @@ import java.util.ArrayList;
 public class WestminsterShoppingManager
 {
     private final int maximum_products = 50;
-    private ArrayList<String> products = new ArrayList<>();
+    private ArrayList<> products = new ArrayList<>();
+
+
+
+
+
+
 
 
 
@@ -39,8 +52,10 @@ public class WestminsterShoppingManager
 
         while(true)
         {
+
             System.out.println("\nEnter Your Option: ");
             String usrInput= sc.next().toUpperCase();
+
         }
 
 
@@ -67,15 +82,72 @@ public class WestminsterShoppingManager
                 C: VIEW PRODUCT LIST
                 D: SAVE DETAILS IN TO A FILE
                                          
-                
+                ---------------------------------------------------------------------------------
                 
                 """);
     }
 
-    public void add_Product()
+    /**
+     * add_Product() method will add a product to the system.
+     */
+
+    private void add_Product()
+    {
+        if (products.size() >= maximum_products)
+        {
+            System.out.println("Product Limit Reached.Cannot add more products.");
+            return;
+        }
+
+        Scanner input=new Scanner(System.in);
+        String productType;
+
+        System.out.println("Enter product type (Electronics/Clothing):  ");
+        productType= input.next();
+
+        switch (productType)
+        {
+            case "electronics":
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+    private void add_Electronic_Product()
     {
 
+        Scanner scanner = new Scanner(System.in);
+        int productId, warrantyYears;
+        String name, brand;
+        double price;
 
+        System.out.println("Enter product ID: ");
+        productId = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter product name: ");
+        name = scanner.nextLine();
+
+        System.out.println("Enter product brand: ");
+        brand = scanner.nextLine();
+
+        System.out.println("Enter product price: ");
+        price = scanner.nextDouble();
+
+        System.out.println("Enter warranty years: ");
+        warrantyYears = scanner.nextInt();
+
+        Electronics newProduct = new Electronics(productId, name, brand, price, warrantyYears);
+        products.add(newProduct);
+        System.out.println(String.format("Product '%s' added successfully.", name));
 
 
 
