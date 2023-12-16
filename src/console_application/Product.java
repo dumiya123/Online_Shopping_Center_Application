c56f475ff0ebe942adc158770a2c708c3aeca9ed
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-12-16 07:46:00 +0530

    initial commit

--- src/console_application/Product.java
+++ src/console_application/Product.java
@@ -2,5 +2,51 @@ package console_application;
 
 public class Product
 {
+    String productID;
+    String name;
+    double price;
 
+    public Product(String productID, String name, double price)
+    {
+
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+
+    }
+
+    public String getProductID()
+    {
+
+        return productID;
+    }
+
+    public void setProductID(String productID)
+    {
+
+        this.productID = productID;
+    }
+
+    public String getName()
+    {
+
+        return name;
+    }
+
+    public void setName(String name)
+    {
+
+        this.name = name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+
+        this.price = price;
+    }
 }
