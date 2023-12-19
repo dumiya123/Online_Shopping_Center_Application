6f4b1cbd2716de878689b8c93c56c0642e610953
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-12-19 17:14:22 +0530

    initial commit

--- src/console_application/Product.java
+++ src/console_application/Product.java
@@ -1,23 +1,23 @@
 package console_application;
 
 /**
- COPYRIGHT (C) Dumindu Induwara Gamage-20221168-w1953846-dumindu.20221168@iit.ac.lk. All Rights Reserved.
- Object-Oriented Programming Coursework L5 sem 1
- @author Dumindu Induwara Gamage
- @version 1 Console application
+ * COPYRIGHT (C) Dumindu Induwara Gamage-20221168-w1953846-dumindu.20221168@iit.ac.lk. All Rights Reserved.
+ * Object-Oriented Programming Coursework L5 sem 1
+ * @author Dumindu Induwara Gamage
+ * @version 1 Console application
  */
 
 /**
- * Product class will be  the parent class for the both Electronic and Clothing classes.
+ * The Product class is the parent class for both Electronic and Clothing classes.
  */
-public class Product
+public abstract class Product
 {
     String productID;
     String name;
     double price;
 
     /**
-     * Default constructor for the  Product class.
+     * Default constructor for the Product class.
      */
     public Product()
     {
@@ -28,104 +28,77 @@ public class Product
      * Constructor for the Product class which accepts one parameter productID.
      * @param productID Specific code for identifying the product.
      */
-    public Product(String productID)
-    {
-
-        this.productID=productID;
-
+    public Product(String productID) {
+        this.productID = productID;
     }
 
     /**
-     * Constructor for the Product class which accepts two parameters productID,name.
+     * Constructor for the Product class which accepts two parameters productID, name.
      * @param productID Specific code for identifying the product.
      * @param name name of the product.
      */
-
-    public Product(String productID,String name)
-    {
-
+    public Product(String productID, String name) {
         this.productID = productID;
         this.name = name;
-
-
     }
 
     /**
-     * Constructor for the Product class which accepts three parameters productID,name,price.
+     * Constructor for the Product class which accepts three parameters productID, name, price.
      * @param productID Specific code for identifying the product.
      * @param name name of the product.
      * @param price Price of the product.
      */
-
-    public Product(String productID, String name, double price)
-    {
-
+    public Product(String productID, String name, double price) {
         this.productID = productID;
         this.name = name;
         this.price = price;
-
     }
 
     /**
-     * getter for the productID
+     * Getter for the productID.
      * @return identification code of the product.
      */
-
-    public String getProductID()
-    {
-
+    public String getProductID() {
         return productID;
     }
 
     /**
-     * setter for the productID
+     * Setter for the productID.
      * @param productID identification code of the product.
      */
-
-    public void setProductID(String productID)
-    {
+    public void setProductID(String productID) {
         this.productID = productID;
     }
 
     /**
-     * getter for the name
+     * Getter for the name.
      * @return name of the product.
      */
-
-    public String getName()
-    {
-
+    public String getName() {
         return name;
     }
 
     /**
-     * setter for the name
-     * @param name  name of the product.
+     * Setter for the name.
+     * @param name name of the product.
      */
-
-    public void setName(String name)
-    {
-
+    public void setName(String name) {
         this.name = name;
     }
 
     /**
-     * getter for the price.
+     * Getter for the price.
      * @return price of the product.
      */
-    public double getPrice()
-    {
+    public double getPrice() {
         return price;
     }
 
     /**
-     * setter for the price.
+     * Setter for the price.
      * @param price price of the product.
      */
-
-    public void setPrice(double price)
-    {
+    public void setPrice(double price) {
         this.price = price;
     }
-
 }
