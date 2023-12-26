b9b48efdb1619fe0524ffd07bbe0189b583bd1bc
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-12-26 22:13:35 +0530

    initial project

--- src/console_application/Electronics.java
+++ src/console_application/Electronics.java
@@ -6,8 +6,73 @@ package console_application;
  @author Dumindu Induwara Gamage
  @version 1 Console application
  */
+
+/**
+ * This is the Electronics subclass .
+ * Electronics subclass inherit from Product class.According to that,Product class is the super class And Parent Class.
+ * This Electronics class hold specific information related to the Electronic Products.
+ */
 public class Electronics extends Product
 {
+    private String brand;
+    private int warranty_duration;
+
+    /**
+     * Default constructor for the Electronics class.
+     */
+    public Electronics()
+    {
+
+    }
+
+    public Electronics(String brand, int warranty_duration)
+    {
+        this.brand = brand;
+        this.warranty_duration = warranty_duration;
+    }
+
+    public Electronics(String productID, String brand, int warranty_duration)
+    {
+
+        super(productID);
+        this.brand = brand;
+        this.warranty_duration = warranty_duration;
+    }
+
+    public Electronics(String productID, String name_of_product, String brand, int warranty_duration)
+    {
+
+        super(productID, name_of_product);
+        this.brand = brand;
+        this.warranty_duration = warranty_duration;
+    }
+
+    public Electronics(String productID, String name_of_product, String available_items, String brand, int warranty_duration)
+    {
+
+        super(productID, name_of_product, available_items);
+        this.brand = brand;
+        this.warranty_duration = warranty_duration;
+    }
+
+    public Electronics(String productID, String name_of_product, String available_items, double price, String brand, int warranty_duration)
+    {
+
+        super(productID, name_of_product, available_items, price);
+        this.brand = brand;
+        this.warranty_duration = warranty_duration;
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
