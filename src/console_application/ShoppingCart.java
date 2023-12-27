122c125849330c70e341b188c57c6e715e575570
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-12-27 11:01:57 +0530

    initial project

--- src/console_application/ShoppingCart.java
+++ src/console_application/ShoppingCart.java
@@ -1,7 +1,31 @@
 package console_application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart
 {
+    private List<Product> products;
+
+    public ShoppingCart()
+    {
+
+        products=new ArrayList<Product>();
+
+    }
+
+    /**
+     *
+     * Declare a method to add product to the cart.
+     *
+     */
+
+
+
+
+
+
+
 
 
 
