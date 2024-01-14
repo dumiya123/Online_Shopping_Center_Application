d68dbd67fca784285680c795a1a4b57b91e15f76
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2024-01-14 06:12:21 +0530

    initial project

--- src/console_application/Clothing.java
+++ src/console_application/Clothing.java
@@ -1,10 +1,10 @@
 package console_application;
 
-/**
+/*
  COPYRIGHT (C) Dumindu Induwara Gamage-20221168-w1953846-dumindu.20221168@iit.ac.lk. All Rights Reserved.
  Object-Oriented Programming Coursework L5 sem 1
  @author Dumindu Induwara Gamage
-// @version 1 Console application
+ @version 1 Console application
  */
 public class Clothing extends Product
 {
--- src/console_application/Product.java
+++ src/console_application/Product.java
@@ -1,7 +1,7 @@
 package console_application;
 
 
-/**
+/*
  * COPYRIGHT (C) Dumindu Induwara Gamage-20221168-w1953846-dumindu.20221168@iit.ac.lk. All Rights Reserved.
  * Object-Oriented Programming Coursework L5 sem 1
  * @author Dumindu Induwara Gamage
